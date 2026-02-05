a56734676179125ca20ca1557a7a6deda1251a47
asifcbe <asifasifcbe@gmail.com>
2026-02-05 21:48:55 +0530

    Added bank details in the invoice template and added .env url changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://apps.ddhost.in/invoice-generator/api',
-        changeOrigin: true,
-        secure: false, // use this if the target uses self-signed SSL cert
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'https://apps.ddhost.in/invoice-generator/api',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
 
 
