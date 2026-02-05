6f9e6017899c09104304d529c70cb15b3fe9b715
asifcbe <asifasifcbe@gmail.com>
2026-02-05 21:58:06 +0530

    Reverted the url changes

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL || 'https://apps.ddhost.in/invoice-generator/api',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        // target: 'https://apps.ddhost.in/invoice-generator/api',
+        target: 'https://engineers-twice-bull-mario.trycloudflare.com/invoice-generator/api',
+        changeOrigin: true,
+        secure: false, // use this if the target uses self-signed SSL cert
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  }
+  },
 })
 
 
